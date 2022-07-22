4d6f6aa4922110983349d3d8179d9e49af1d5e2d
Adam Siwiec <asiwiec@nvidia.com>
2022-07-22 01:59:15 -0400

    update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Adam Siwiec',
+    title: 'Adam Siwiec | Software Engineer | Web Developer',
     description:
       'Adam Siwiec is a software engineer specializing in building artificial intelligence, blockchain, and cloud products.',
     siteUrl: 'https://siwiec.us', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@asiwiec',
+    twitterUsername: '@asiwiec1',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
